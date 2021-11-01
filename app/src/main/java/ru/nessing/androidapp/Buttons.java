43461b24ef4c2615d5e134d77cc5b4cac6d1a70d
Nessing <35237174+Nessing@users.noreply.github.com>
2021-11-01 19:57:50 +0300

    Created new class Calculations

--- app/src/main/java/ru/nessing/androidapp/Buttons.java
+++ app/src/main/java/ru/nessing/androidapp/Buttons.java
@@ -1,7 +1,6 @@
 package ru.nessing.androidapp;
 
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,38 +9,20 @@ public class Buttons extends AppCompatActivity implements View.OnClickListener {
 
     private TextView fieldInput;
     private String out = "";
-    private float result;
-    private boolean oneOperation, negativeNum, decimal, decimalSecond;
-    private Button button1, button2, button3, button4,
-            button5, button6, button7, button8,
-            button9, button0, buttonDecimal, buttonCancel,
-            buttonPlus, buttonMinus, buttonMultiply, buttonDivide, buttonEquals;
+    private Calculations calculations;
 
+    private final int[] numberButtonIds = new int[]{
+            R.id.button_1, R.id.button_2, R.id.button_3,
+            R.id.button_4, R.id.button_5, R.id.button_6,
+            R.id.button_7, R.id.button_8, R.id.button_9,
+            R.id.button_zero, R.id.button_decimal, R.id.button_cancel,
+            R.id.button_plus, R.id.button_minus, R.id.button_multiply,
+            R.id.button_decimal, R.id.button_divide, R.id.button_equals
+    };
 
     protected void create(MainActivity mainActivity) {
-        oneOperation = false;
-        negativeNum = false;
-        decimal = false;
-        decimalSecond = false;
+        calculations = new Calculations();
 
-        button1 = mainActivity.findViewById(R.id.button_1);
-        button2 = mainActivity.findViewById(R.id.button_2);
-        button3 = mainActivity.findViewById(R.id.button_3);
-        button4 = mainActivity.findViewById(R.id.button_4);
-        button5 = mainActivity.findViewById(R.id.button_5);
-        button6 = mainActivity.findViewById(R.id.button_6);
-        button7 = mainActivity.findViewById(R.id.button_7);
-        button8 = mainActivity.findViewById(R.id.button_8);
-        button9 = mainActivity.findViewById(R.id.button_9);
-        button0 = mainActivity.findViewById(R.id.button_zero);
-        buttonDecimal = mainActivity.findViewById(R.id.button_decimal);
-
-        buttonCancel = mainActivity.findViewById(R.id.button_cancel);
-        buttonPlus = mainActivity.findViewById(R.id.button_plus);
-        buttonMinus = mainActivity.findViewById(R.id.button_minus);
-        buttonMultiply = mainActivity.findViewById(R.id.button_multiply);
-        buttonDivide = mainActivity.findViewById(R.id.button_divide);
-        buttonEquals = mainActivity.findViewById(R.id.button_equals);
         createButtons(mainActivity);
     }
 
@@ -56,28 +37,18 @@ public class Buttons extends AppCompatActivity implements View.OnClickListener {
     private void createButtons(MainActivity mainActivity) {
         fieldInput = mainActivity.findViewById(R.id.fieldInput);
 
-        button1.setOnClickListener(this);
-        button2.setOnClickListener(this);
-        button3.setOnClickListener(this);
-        button4.setOnClickListener(this);
-        button5.setOnClickListener(this);
-        button6.setOnClickListener(this);
-        button7.setOnClickListener(this);
-        button8.setOnClickListener(this);
-        button9.setOnClickListener(this);
-        button0.setOnClickListener(this);
-        buttonDecimal.setOnClickListener(this);
+        setNumberButtonIdsListeners(mainActivity);
+    }
 
-        buttonCancel.setOnClickListener(this);
-        buttonPlus.setOnClickListener(this);
-        buttonMinus.setOnClickListener(this);
-        buttonMultiply.setOnClickListener(this);
-        buttonDivide.setOnClickListener(this);
-        buttonEquals.setOnClickListener(this);
+    private void setNumberButtonIdsListeners(MainActivity mainActivity) {
+        for (int i = 0; i < numberButtonIds.length; i++) {
+            mainActivity.findViewById(numberButtonIds[i]).setOnClickListener(this);
+        }
     }
 
     @Override
     public void onClick(View view) {
+
         if (view.getId() == R.id.button_1) {
             out += 1;
             fieldInput.setText(out);
@@ -109,90 +80,51 @@ public class Buttons extends AppCompatActivity implements View.OnClickListener {
             out += 0;
             fieldInput.setText(out);
         } else if (view.getId() == R.id.button_decimal) {
-            if (!decimal && out.length() >= 1 && !negativeNum || !oneOperation && out.length() >= 3 && negativeNum || decimalSecond) {
+            if (calculations.decimal(out.length())) {
                 out += ".";
                 fieldInput.setText(out);
-                decimal = true;
-                decimalSecond = false;
             }
 
             // operations
         } else if (view.getId() == R.id.button_multiply) {
-            if (!oneOperation && out.length() >= 1 && !negativeNum || !oneOperation && out.length() >= 3 && negativeNum) {
+            if (calculations.multiply(out.length())) {
                 out += " * ";
                 fieldInput.setText(out);
-                oneOperation = true;
-                decimal = false;
-                decimalSecond = true;
             }
         } else if (view.getId() == R.id.button_divide) {
-            if (!oneOperation && out.length() >= 1 && !negativeNum || !oneOperation && out.length() >= 3 && negativeNum) {
+            if (calculations.divide(out.length())) {
                 out += " / ";
                 fieldInput.setText(out);
-                oneOperation = true;
-                decimal = false;
-                decimalSecond = true;
             }
 
         } else if (view.getId() == R.id.button_plus) {
-            if (!oneOperation && out.length() >= 1 && !negativeNum || !oneOperation && out.length() >= 3 && negativeNum) {
+            if (calculations.plus(out.length())) {
                 out += " + ";
                 fieldInput.setText(out);
-                oneOperation = true;
-                decimal = false;
-                decimalSecond = true;
             }
         } else if (view.getId() == R.id.button_minus) {
-            if (out.length() < 1) {
-                out += " -";
-                fieldInput.setText(out);
-                negativeNum = true;
-            } else if (!oneOperation) {
-                out += " - ";
-                fieldInput.setText(out);
-                oneOperation = true;
-                decimalSecond = true;
-            }
+            out += calculations.minus(out.length());
+            fieldInput.setText(out);
         } else if (view.getId() == R.id.button_equals) {
-            String[] arr = out.trim().split(" ");
             try {
-                if (arr[1].equals("+")) {
-                    result = Float.parseFloat(arr[0]) + Float.parseFloat(arr[2]);
-                } else if (arr[1].equals("-")) {
-                    result = Float.parseFloat(arr[0]) - Float.parseFloat(arr[2]);
-                } else if (arr[1].equals("*")) {
-                    result = Float.parseFloat(arr[0]) * Float.parseFloat(arr[2]);
-                } else if (arr[1].equals("/")) {
-                    result = Float.parseFloat(arr[0]) / Float.parseFloat(arr[2]);
-                }
-                fieldInput.setText(String.valueOf(result));
-                out = String.valueOf(result);
-
-                setDefaultFlags();
-
+                fieldInput.setText(String.valueOf(calculations.equals(out)));
+                out = String.valueOf(calculations.equals(out));
             } catch (ArrayIndexOutOfBoundsException e) {
                 System.err.println("expression don't right");
+                fieldInput.setText("expression don't right");
+                out = "";
+                calculations.clean();
             } catch (NumberFormatException e) {
                 System.err.println("incorrect expression");
-                fieldInput.setText("");
+                fieldInput.setText("incorrect expression");
                 out = "";
-                result = 0;
-
-                setDefaultFlags();
+                calculations.clean();
             }
 
         } else if (view.getId() == R.id.button_cancel) {
-            out = "";
             fieldInput.setText(out);
-            result = 0;
-            setDefaultFlags();
+            out = "";
+            calculations.clean();
         }
-    }
-
-    private void setDefaultFlags() {
-        oneOperation = false;
-        negativeNum = false;
-        decimal = false;
-        decimalSecond = false;
     }
 }
